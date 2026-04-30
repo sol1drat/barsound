23056132e21dceb05f30e334241187efa985f062
byynine <nutitota@tutanota.com>
2026-04-30 18:40:08 +0400

    first test

--- public/app.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js")
-            .then(reg => console.log("[info] service worker registered:", reg.scope))
-            .catch(err => console.error("service worker failed:", err));
-    });
-}
