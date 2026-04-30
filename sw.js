8285d3a7185e74f975cc66e3450ecfcf62032004
byynine <nutitota@tutanota.com>
2026-04-30 19:41:04 +0400

    update

--- sw.js
+++ sw.js
@@ -1,29 +1,40 @@
-const CACHE = 'barsound-shell-v2';
+const CACHE = 'barsound-shell-v3';
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/manifest.json',
-  // do not cache dynamic blobs from IndexedDB
+  './',
+  'index.html',
+  'style.css',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
+  'icons/maskable-512.png',
+  'icons/apple-180.png'
 ];
 
 self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  evt.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', evt => {
   evt.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', evt => {
-  // ignore requests for object URLs and IndexedDB data (they're not network requests)
   evt.respondWith(
-    caches.match(evt.request).then(cached => cached || fetch(evt.request).catch(() => cached))
+    caches.match(evt.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(evt.request).catch(() => {
+        return new Response('', { status: 503, statusText: 'Offline' });
+      });
+    })
   );
 });
